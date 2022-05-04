b640227727ed30d4e526b53276de1c57685aab20
Dchamel <dchamel@yandex.ru>
2022-05-04 20:06:31 +0400

    p1-49, the beginning of book

---
+++ MichaelDawsonBook/p1-44/p1-44.py
@@ -0,0 +1,10 @@
+#p42-1
+print('Ice cream')
+a = input('Input type of the ice cream:')
+print(a)
+#p42-2
+print('My name is Denis')
+input('Press any key')
+#p42-3
+print('Timeo Danaos et dona ferentes\nVirgil "Aeneid"')
+
