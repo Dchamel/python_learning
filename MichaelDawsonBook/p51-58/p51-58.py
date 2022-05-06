9e3fe2c3f2b2701f41a1dea9e2436623a414105d
Dchamel <dchamel@yandex.ru>
2022-05-06 21:00:35 +0400

    p51-58, variables, beginning

---
+++ MichaelDawsonBook/p51-58/p51-58.py
@@ -0,0 +1,26 @@
+print('Two strings' + ' in one')
+print('Some lines'+' at the bottom.\n'\
+      +'Jeden Python'+' Zwei Eggs.\n'\
+      +'A lots of letters. Or a small amount')
+#Repeat
+print('O'+'lo'*5)
+
+#p53 Text tasks
+print('100-50 =',100-50)
+print('100*50 =',100*50)
+print('100/50 =',100/50) #Float type
+print('100+50 =',100+50)
+
+print('10 / 3 =',10/3)
+print('10 // 3 =',10//3)
+print('10 % 3 =',10%3)
+
+print('19 / 4 =',19/4)
+print('19 // 4 =',19//4)
+print('19 % 4 =',19%4)
+
+#p56 Hello
+name = 'Shon'
+print(name)
+print('Hello,',name)
+
