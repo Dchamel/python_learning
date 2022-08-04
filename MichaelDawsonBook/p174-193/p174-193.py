c38fdc2d96fe0d9360d7bea1bc57957888935e4e
Dchamel <dchamel@yandex.ru>
2022-08-04 16:42:52 +0400

    p174-189, functions, pseudocode, how it works, constants, tuple, tic-tac-toe, end of chapter

--- MichaelDawsonBook/p174-193/p174-193.py
+++ MichaelDawsonBook/p174-193/p174-193.py
@@ -28,7 +28,7 @@ named(q='1',w='2')
 #     change turn
 # congrads the winner
 
-def display_instruct() #show instruction
+def display_instruct():
     '''Instruction for the player'''
     print('''Welcome to Tic-Tac-Toe Game
      0 | 1 | 2 
@@ -52,6 +52,7 @@ def ask_number(question, low, high):
         response = int(input(question))
     return response
 
+
 def pieces():
     '''whose first move'''
     go_first = ask_yes_no('Do you want to do the First Move ? (y/n): ')
@@ -65,12 +66,14 @@ def pieces():
         human = O
     return computer, human
 
+
 def new_board():
     '''empty board and return it'''
     board = []
     for square in range(NUM_SQUARES):
         board.append(EMPTY)
     return board
+
 
 def display_board(board):
     '''shows board'''
@@ -82,7 +85,7 @@ def display_board(board):
 
 def legal_moves(board):
     '''Takes board, return - list of moves'''
-    moves=[]
+    moves = []
     for square in range(NUM_SQUARES):
         if board[square] == EMPTY:
             moves.append(square)
@@ -103,8 +106,10 @@ def winner(board):
         if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
             winner = board[row[0]]
             return winner
-        if EMPTY not in board:
-            return TIE
+
+    if EMPTY not in board:
+        return TIE
+
     return None
 
 def human_move(board, human):
@@ -118,12 +123,83 @@ def human_move(board, human):
     print('Okay...')
     return move
 
-computer_move(board, computer, human) #calc computer move
-next_turn(turn) #takes type of chips and return it
-congrat_winner(the_winner, computer, human) #congrats the winner
+
+def computer_move(board, computer, human):
+    '''calc computer move'''
+    board = board[:]
+    BEST_MOVES = (4, 0, 2, 6, 8, 1, 3, 5, 7)
+    print ('I`ll chose', end=' ')
+
+    for move in legal_moves(board):
+        board[move] = computer
+        if winner(board) == computer:
+            print(move)
+            return move
+
+        board[move] = EMPTY
+
+    for move in legal_moves(board):
+        board[move] = human
+        if winner(board) == human:
+            print(move)
+            return move
+
+        board[move] = EMPTY
+
+    for move in BEST_MOVES:
+        if move in legal_moves(board):
+            print(move)
+            return move
+
+
+def next_turn(turn):
+    '''takes type of chips and return it'''
+    if turn == X:
+        return O
+    else:
+        return X
+
+
+def congrat_winner(the_winner, computer, human):
+    '''congrats the winner'''
+    if the_winner != TIE:
+        print(f'3 {the_winner} in line!\n')
+    else:
+        print('No winner !')
+    if the_winner == computer:
+        print('Computer Wins !')
+    elif the_winner == human:
+        print('Human Wins !')
+    elif the_winner == TIE:
+        print('Nooooo winner ! ')
+
+
+def main():
+    display_instruct()
+    computer, human = pieces()
+    turn = X
+    board = new_board()
+    display_board(board)
+
+    while not winner(board):
+        if turn == human:
+            move = human_move(board, human)
+            board[move] = human
+        else:
+            move = computer_move(board, computer, human)
+            board[move] = computer
+        display_board(board)
+        turn = next_turn(turn)
+
+    the_winner = winner(board)
+    congrat_winner(the_winner, computer, human)
+
 
 X = 'X'
 O = 'O'
 EMPTY = ' '
 TIE = 'Draw'
 NUM_SQUARES = 9
+
+main()
+input('Press Enter for EXIT')
