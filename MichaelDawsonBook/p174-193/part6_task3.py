a45aa56fb89c90572afa12e723bd6a31f33eef4a
Dchamel <dchamel@yandex.ru>
2022-08-06 18:20:59 +0400

    p193, ending part6, task3

---
+++ MichaelDawsonBook/p174-193/part6_task3.py
@@ -0,0 +1,32 @@
+#guess the number
+import random
+
+
+def ask_number(question, low, high):
+    '''ask user for number in range, return - integer'''
+    response = ''
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+def main():
+    low = 1
+    high = 100
+    number = random.randint(low, high)
+    guess = ask_number('Input Number: ', low, high)
+    tries = 1
+    max_tries = 7
+    while number != guess and tries <= max_tries-1:
+        if guess < number:
+            print('Mine is more')
+        else:
+            print('Mine is less')
+        guess = ask_number('Input Number: ', low, high)
+        tries += 1
+    if tries >= max_tries:
+        print('You Lose. Try again.')
+    else:
+        print('Correct ! Number of your tries is:', tries)
+
+main()
