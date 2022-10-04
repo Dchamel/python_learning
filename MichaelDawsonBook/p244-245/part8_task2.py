6dced4b3e7001a94a0a54496f6ae2185ed48ed8f
Dchamel <dchamel@yandex.ru>
2022-10-04 23:35:48 +0400

    p244, task2

--- MichaelDawsonBook/p244-245/part8_task2.py
+++ MichaelDawsonBook/p244-245/part8_task2.py
@@ -1,4 +1,4 @@
-import keyboard
+import keyboard, time
 
 class Tv(object):
     '''Tv Control'''
@@ -8,6 +8,8 @@ class Tv(object):
         self.channel = channel
         if self.channel < 1:
             self.channel = 1
+        elif self.channel > 100:
+            self.channel = 100
         return channel
 
     def changeVolume(self, volume = 20):
@@ -16,12 +18,32 @@ class Tv(object):
         while True:
             if keyboard.is_pressed('up'):
                 volume += 2
+                time.sleep(0.1)
                 print(f'Volume UP\nNow volume is {volume}')
+                if volume > 98:
+                    volume = 98
             elif keyboard.is_pressed('down'):
-                print('DOWN')
-            elif keyboard.is_pressed('Escape'):
+                volume -= 2
+                time.sleep(0.1)
+                print(f'Volume DOWN\nNow volume is {volume}')
+                if volume < 2:
+                    volume = 2
+            elif keyboard.is_pressed('escape'):
                 print('Exit')
-                break
+                return volume
 
 q1 = Tv()
-q1.changeVolume()
+userInput = ''
+while userInput != '0':
+    print('''
+    Welcome to TV remote control
+    0 - Exit
+    1 - Change Channel
+    2 - Change Volume''')
+    userInput = input('Choice: ')
+    if userInput == '1':
+        print(f'You now at {q1.chooseChannel()} channel')
+    elif userInput == '2':
+        print(f'Volume of the Tv now is {q1.changeVolume()}')
+
+
