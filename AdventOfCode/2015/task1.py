639946b8a256ad1c94931249d3c3600459f1ae80
Dchamel <dchamel@yandex.ru>
2022-10-17 21:36:26 +0400

    task1 - add Unit Tests

--- AdventOfCode/2015/task1.py
+++ AdventOfCode/2015/task1.py
@@ -1,15 +1,30 @@
-def readData():
-    with open('inputs/task1.txt', 'r') as rawData:
+import unittest
+
+def readData(path):
+    with open(path, 'r') as rawData:
         data = rawData.read()
     return data
 
-def task1():
+def task1(data):
     lvl = 0
-    for each in readData():
+    for each in data:
         if each == '(':
             lvl += 1
         else:
             lvl -= 1
     return lvl
 
-print(task1())
+class AllTestsTask1(unittest.TestCase):
+    def test_readData(self):
+        actual = readData('inputs/task1.txt')[1]
+        expected = '('
+        self.assertEqual(actual, expected)
+
+    def test_task1(self):
+        actual = task1(')((')
+        expected = 1
+        self.assertEqual(actual, expected)
+
+path = 'inputs/task1.txt'
+data = readData(path)
+print(task1(data))
