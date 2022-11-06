fc8c9f41464492f7d83e4c90ab736ebd8c99fcb5
Dchamel <dchamel@yandex.ru>
2022-11-06 23:55:00 +0400

    p266-267, tmp2, classes design

--- MichaelDawsonBook/p266-267/p266-267.py
+++ MichaelDawsonBook/p266-267/p266-267.py
@@ -2,27 +2,31 @@ import unittest, random, cards
 from time import perf_counter
 t1 = perf_counter()
 
+print('Welcome to the Black Jack v1.0')
 
-print('Welcome to the Simple Game')
-again = None
-while again != 'n':
-    players = []
-    num = games.ask_number(question='How many Players in the Game ? (2-5):', low=2, high=5)
 
-    for i in range(num):
-        name = input('Your Name: ')
-        score = random.randrange(100) + 1
-        player = games.Player(name, score)
-        players.append(player)
 
-    print('Game Results: ')
-    for player in players:
-        print(player)
+class BJ_Card(cards.Card):
+    pass
 
-    again = games.ask_yes_no('Again? (y/n): ')
+class BJ_Deck(cards.Deck):
+    pass
+
+class BJ_Hand(cards.Hand):
+    pass
+
+class BJ_Player(BJ_Hand):
+    pass
+
+class BJ_Dealer(BJ_Hand):
+    pass
+
+class BJ_Game(object):
+
+
+
 
 input('Press Enter for Exit')
-
 
 # Test Section
 class AllTests(unittest.TestCase):
