f8fb5761a5166e928f9f90c3dbfac20e3474c3dd
Dchamel <dchamel@yandex.ru>
2022-11-19 04:41:42 +0400

    Advent of Code 2015, task3  - task3_part1  - task3_part2  - unit tests for task3

--- AdventOfCode/2015/task3.py
+++ AdventOfCode/2015/task3.py
@@ -31,35 +31,24 @@ def delivery_path(data: str) -> set:
         x, y = santa_step(direction, x, y)
         coord = (x, y)
         delivery_path.add(coord)
-
-    print(delivery_path)
     return delivery_path
-
-def delivered_presents(delivery_path):
-    return len(delivery_path)
 
 
 def two_ways(data):
     data_santa = ''
     data_robo_santa = ''
     for index, direction in enumerate(data, start=0):
-        if index == 0:
+        if index % 2 != 0:  # odd
             data_santa += direction
+        else:  # even
             data_robo_santa += direction
-            print(f'Santa (Index0/Data): {index, data_santa}')
-            print(f'Robo Santa (Index0/Data): {index, data_robo_santa}')
-        elif index % 2 != 0: #odd
-            data_santa += direction
-            print(f'Santa (Index/Data): {index, data_santa}')
-        else: #even
-            data_robo_santa += direction
-            print(f'Robo Santa (Index/Data): {index, data_robo_santa}')
     return data_santa, data_robo_santa
 
 
 # tests
 class AllTestsTask3(unittest.TestCase):
 
+    # tests for part1
     def test1_read_data(self):
         expected = 'v'
         actual = read_data('inputs/task3.txt')[3]
@@ -72,43 +61,51 @@ class AllTestsTask3(unittest.TestCase):
 
     def test3_delivered_presents1(self):
         expected = 2
-        actual = delivered_presents('>')
+        actual = len(delivery_path('>'))
         self.assertEqual(expected, actual)
 
     def test3_delivered_presents2(self):
         expected = 4
-        actual = delivered_presents('^>v<')
+        actual = len(delivery_path('^>v<'))
         self.assertEqual(expected, actual)
 
     def test3_delivered_presents3(self):
         expected = 2
-        actual = delivered_presents('^v^v^v^v^v')
+        actual = len(delivery_path('^v^v^v^v^v'))
+        self.assertEqual(expected, actual)
+
+    # tests for part2
+    def test4_delivered_presents_santa_and_robo1(self):
+        expected = 3
+        data_santa, data_robo_santa = two_ways('^v')
+        actual = len(delivery_path(data_santa).union(delivery_path(data_robo_santa)))
+        self.assertEqual(expected, actual)
+
+    def test4_delivered_presents_santa_and_robo2(self):
+        expected = 3
+        data_santa, data_robo_santa = two_ways('^>v<')
+        actual = len(delivery_path(data_santa).union(delivery_path(data_robo_santa)))
+        self.assertEqual(expected, actual)
+
+    def test4_delivered_presents_santa_and_robo3(self):
+        expected = 11
+        data_santa, data_robo_santa = two_ways('^v^v^v^v^v')
+        actual = len(delivery_path(data_santa).union(delivery_path(data_robo_santa)))
         self.assertEqual(expected, actual)
 
 
 path = 'inputs/task3.txt'
-# data = read_data(path)
-data = '^v'
-data_santa, data_robo_santa = two_ways(data)
-print(data_santa)
-print(data_robo_santa)
+data = read_data(path)
 
+# part1
+print(f'Delivered presents by Santa: {len(delivery_path(data))}')
+
+# part2
+data_santa, data_robo_santa = two_ways(data)
 delivery_path_santa = delivery_path(data_santa)
 delivery_path_robo_santa = delivery_path(data_robo_santa)
-
 new_path = delivery_path_santa.union(delivery_path_robo_santa)
-print(new_path)
-print(len(new_path))
-
-
-
-# delivery_path = delivery_path(data)
-# q = delivered_presents(data_santa)
-# w = delivered_presents(data_robo_santa)
-# c = delivered_presents('^v')
-# print(f'Test: {c}')
-# print(f'Santa:{q} Robo Santa: {w}')
-# print(f'Delivered presents: {q+w}')
+print(f'Delivered presents by Santa and his Robo: {len(new_path)}')
 
 t2 = perf_counter()
 print(f'{t2 - t1:.5f} sec')
