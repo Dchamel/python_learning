7d1005fe0d49d14af704d4d950f73f4dfec687d1
Dchamel <dchamel@yandex.ru>
2022-12-21 15:20:39 +0400

    Advent of Code 2022, task6 - part1 - part2 - unit tests

--- AdventOfCode/2022/task6.py
+++ AdventOfCode/2022/task6.py
@@ -22,15 +22,70 @@ def part_two(data):
 
 
 # tests - not implemented
-class AllTests2022Task2(unittest.TestCase):
+class AllTests2022Task6(unittest.TestCase):
 
     def setUp(self) -> None:
         self.path = path
-        self.data = ''
+
+        self.data = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+        self.data2 = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+        self.data3 = 'nppdvjthqldpwncqszvftbrmjlhg'
+        self.data4 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+        self.data5 = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
 
     def test01_read_data(self):
-        expected = 's'
+        expected = 'h'
         actual = read_data(self.path)[3]
+        self.assertEqual(expected, actual)
+
+    def test02_part_one(self):
+        expected = 7
+        actual = part_one(self.data)
+        self.assertEqual(expected, actual)
+
+    def test03_part_one(self):
+        expected = 5
+        actual = part_one(self.data2)
+        self.assertEqual(expected, actual)
+
+    def test04_part_one(self):
+        expected = 6
+        actual = part_one(self.data3)
+        self.assertEqual(expected, actual)
+
+    def test05_part_one(self):
+        expected = 10
+        actual = part_one(self.data4)
+        self.assertEqual(expected, actual)
+
+    def test06_part_one(self):
+        expected = 11
+        actual = part_one(self.data5)
+        self.assertEqual(expected, actual)
+
+    def test07_part_two(self):
+        expected = 19
+        actual = part_two(self.data)
+        self.assertEqual(expected, actual)
+
+    def test08_part_two(self):
+        expected = 23
+        actual = part_two(self.data2)
+        self.assertEqual(expected, actual)
+
+    def test09_part_two(self):
+        expected = 23
+        actual = part_two(self.data3)
+        self.assertEqual(expected, actual)
+
+    def test10_part_two(self):
+        expected = 29
+        actual = part_two(self.data4)
+        self.assertEqual(expected, actual)
+
+    def test11_part_two(self):
+        expected = 26
+        actual = part_two(self.data5)
         self.assertEqual(expected, actual)
 
 
