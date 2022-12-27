6257c4f112ec94403b5234f1ee92b25ad9230ab8
Dchamel <dchamel@yandex.ru>
2022-12-27 23:40:36 +0400

    VK Parser - Group News - v0.35 - architecture for writing (in progress) - was added datetime for dump files - data preparing for csv-writer (in progress) - unit tests - not implemented

--- MyProjects/VK Parser/parser.py
+++ MyProjects/VK Parser/parser.py
@@ -20,19 +20,31 @@ t1 = perf_counter()
 def read_data(path):
     file_name = auth_keys.group_name
     current_date = date.today().strftime('%d%m%Y')
-    with open(path + file_name + current_date + '.csv', 'r') as rawDataCsv:
-        return rawData.readlines()
+    with open(path + file_name + current_date + '.csv', 'r', encoding='utf-8') as file:
+        data = list(csv.reader(file))
+    return data
 
 
 def get_all_from_wall(groupid):
-    data = vk_api.wall.get(domain=groupid, count=2, v=auth_keys.api_ver)
-    return data
+    data_vk = vk_api.wall.get(domain=groupid, count=5, v=auth_keys.api_ver)
+    # get all users id
+    from_id_list = []
+    for each in data_vk['items']:
+        from_id_list.append(str(each['from_id']))
+    from_id_string = ','.join(from_id_list)
+    data_vk_from_id = vk_api.users.get(user_ids=from_id_string, v=auth_keys.api_ver)
+    # print(data_vk_from_id)
+    print(data_vk_from_id)
+
+    # deleting links, trash-info and other photos
+
+    return data_vk
 
 
 def write_data(path, all_data_from_wall):
     file_name = auth_keys.group_name
     current_date = date.today().strftime('%d%m%Y')
-    with open(path + file_name + current_date + '.csv', 'w', encoding='utf-8') as file:
+    with open(path + file_name + current_date + '.csv', 'w', encoding='utf-8', newline='') as file:
         csvwriter = csv.writer(file)
         csvwriter.writerow(('post_id', 'from_user', 'date', 'text', 'attachment'))
         for linedata in all_data_from_wall['items']:
@@ -46,7 +58,8 @@ vk_api = vk.API(access_token=auth_keys.api_token)
 all_data_from_wall = get_all_from_wall(auth_keys.group_name)
 write_data(auth_keys.path, all_data_from_wall)
 
-read_data(auth_keys.path)
+rawData = read_data(auth_keys.path)
+print(rawData)
 
 
 # tests - not implemented
