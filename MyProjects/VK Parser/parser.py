8d823d3d0e4dea1895b0e1ace2d573bb7e42c28d
Dchamel <dchamel@yandex.ru>
2023-01-04 05:59:23 +0400

    VK Parser - Group News - v0.5 - architecture for writing (in progress) - was added datetime for dump files - data preparing for csv-writer (in progress) - module for processing data (the beginning) - filter attachments - from_user now Name and Surname - unit tests - not implemented

--- MyProjects/VK Parser/parser.py
+++ MyProjects/VK Parser/parser.py
@@ -26,7 +26,7 @@ def read_data(path):
 
 
 def get_all_from_wall(groupid):
-    data_vk = vk_api.wall.get(domain=groupid, count=3, v=auth_keys.api_ver)
+    data_vk = vk_api.wall.get(domain=groupid, count=10, v=auth_keys.api_ver)
 
     # get all users id and filter attachments
     from_id_list = []
@@ -38,12 +38,24 @@ def get_all_from_wall(groupid):
             data_vk['items'][index]['attachments'] = each['attachments'][0]['photo']['sizes'][-1]['url']
         except:
             continue
-    print(data_vk)
+    # print(data_vk)
+    from_id_list = list(set(from_id_list))
     from_id_string = ','.join(from_id_list)
-    data_vk_from_id = vk_api.users.get(user_ids=from_id_string, v=auth_keys.api_ver)
-    # print(data_vk_from_id)
+    data_vk_from_id = vk_api.users.get(user_ids=from_id_string, v=auth_keys.api_ver, lang=auth_keys.lang)
+    # print(from_id_list)
     # print(data_vk_from_id)
 
+    # iteration data_vk again just for change 'id' to 'names'
+    for index, each in enumerate(data_vk['items']):
+        if str(each['from_id']) == auth_keys.group_id:
+            data_vk['items'][index]['from_id'] = auth_keys.group_name_ru
+        else:
+            for each_user_name in data_vk_from_id:
+                print(each_user_name['id'])
+                if each_user_name['id'] == each['from_id']:
+                    full_name = each_user_name['first_name'] + ' ' + each_user_name['last_name']
+                    data_vk['items'][index]['from_id'] = full_name
+    print(data_vk)
     return data_vk
 
 
@@ -54,7 +66,7 @@ def write_data(path, all_data_from_wall):
         csvwriter = csv.writer(file)
         csvwriter.writerow(('post_id', 'from_user', 'date', 'text', 'attachment'))
         for linedata in all_data_from_wall['items']:
-            linedata['from_id'] = int(str(linedata['from_id'])[1:])
+            linedata['from_id'] = linedata['from_id']
             csvwriter.writerow((linedata['id'], linedata['from_id'], linedata['date'], linedata['text'],
                                 linedata['attachments']))
 
