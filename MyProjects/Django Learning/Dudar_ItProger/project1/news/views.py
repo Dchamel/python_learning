154dffbfe34f549a920abbe72bbafdc9b9e4b23d
Dchamel <dchamel@yandex.ru>
2023-01-17 20:47:14 +0400

    Django Learning - Dudar_ItProger -- project1 (lesson 10)

--- MyProjects/Django Learning/Dudar_ItProger/project1/news/views.py
+++ MyProjects/Django Learning/Dudar_ItProger/project1/news/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.order_by('-date');
     return render(request, 'news/news_home.html', {'news': news})
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail_view.html'
+    context_object_name = 'article'
 def create(request):
     error = ''
     if request.method == 'POST':
