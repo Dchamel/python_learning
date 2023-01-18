72447a4941c01f6eb09fb4ffc27d027ac84e8a7d
Dchamel <dchamel@yandex.ru>
2023-01-18 19:17:28 +0400

    Django Learning - Dudar_ItProger -- project1 (lesson 11) -- end

--- MyProjects/Django Learning/Dudar_ItProger/project1/news/views.py
+++ MyProjects/Django Learning/Dudar_ItProger/project1/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Articles.objects.order_by('-date');
@@ -11,6 +11,18 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/detail_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+
 def create(request):
     error = ''
     if request.method == 'POST':
