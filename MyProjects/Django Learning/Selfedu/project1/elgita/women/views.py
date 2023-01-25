e1e9e34ff48e0f0b1f35b89531e8a85750b6552a
Dchamel <dchamel@yandex.ru>
2023-01-25 23:24:54 +0400

    - Selfedu -- project1 (lesson 9)

--- MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
@@ -10,10 +10,14 @@ menu = [{'title': "About", 'url_name': 'about'},
 ]
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Main page'
+        'title': 'Main page',
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -31,6 +35,23 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Showing an Article with id: {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'All Categories',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>page not found</h1>')
