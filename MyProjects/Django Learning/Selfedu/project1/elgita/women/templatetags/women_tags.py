8ec65d81b550bf2942ab3846a446b3f4485e36df
Dchamel <dchamel@yandex.ru>
2023-01-27 03:14:04 +0400

    - Selfedu -- project1 (lesson 11)

---
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/templatetags/women_tags.py
@@ -0,0 +1,28 @@
+from django import template
+from women.models import *
+
+register = template.Library()
+
+@register.simple_tag(name='getcats')
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    return {'cats': cats, 'cat_selected': cat_selected}
+
+@register.inclusion_tag('women/main_menu.html')
+def main_menu():
+    menu = [{'title': "About", 'url_name': 'about'},
+            {'title': "Add Article", 'url_name': 'add_page'},
+            {'title': "Feedback", 'url_name': 'contact'},
+            {'title': "Login", 'url_name': 'login'}
+            ]
+    return {'menu': menu}
--- MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
@@ -10,11 +10,9 @@ menu = [{'title': "About", 'url_name': 'about'},
 ]
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Main page',
         'cat_selected': 0,
@@ -39,14 +37,12 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'All Categories',
         'cat_selected': cat_id,
