5a22d37cd50e637a5c755227904e2aced3577118
Dchamel <dchamel@yandex.ru>
2023-02-06 22:37:42 +0400

    - Selfedu -- project1 (lesson 12)

--- MyProjects/Django Learning/Selfedu/project1/elgita/women/models.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/models.py
@@ -4,18 +4,19 @@ from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=25, verbose_name='Title')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Categories')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Famous Women'
@@ -25,11 +26,12 @@ class Women(models.Model):
 class Category(models.Model):
 
     name = models.CharField(max_length=100, db_index=True, verbose_name='Category')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
     class Meta:
         verbose_name = 'Category'
--- MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/views.py
@@ -30,8 +30,8 @@ def contact(request):
 def login(request):
     return HttpResponse('Login')
 
-def show_post(request, post_id):
-    post = get_object_or_404(Women, pk=post_id)
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
 
     context = {
         'post': post,
@@ -42,8 +42,8 @@ def show_post(request, post_id):
 
     return render(request, 'women/post.html', context=context)
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(slug=cat_slug)
 
     if len(posts) == 0:
         raise Http404()
@@ -51,7 +51,7 @@ def show_category(request, cat_id):
     context = {
         'posts': posts,
         'title': 'All Categories',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
     return render(request, 'women/index.html', context=context)
 
