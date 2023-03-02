5192d2256bd084ddecb798835ebdba9123e0aa66
Dchamel <dchamel@yandex.ru>
2023-03-02 23:50:20 +0400

    - Selfedu -- project1 (lesson 22)

--- MyProjects/Django Learning/Selfedu/project1/elgita/women/utils.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 
 from women.models import *
 
@@ -13,7 +14,10 @@ class DataMixin:
     def get_user_context(self, **kwargs):
         context = kwargs
         # cats = Category.objects.all()
-        cats = Category.objects.annotate(Count('women'))
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
