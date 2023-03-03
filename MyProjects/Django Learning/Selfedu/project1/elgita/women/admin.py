e7f500c40fa8170cf6f714068d23146e626b72f2
Dchamel <dchamel@yandex.ru>
2023-03-04 00:40:08 +0400

    - Selfedu -- project1 (lesson 24)

--- MyProjects/Django Learning/Selfedu/project1/elgita/women/admin.py
+++ MyProjects/Django Learning/Selfedu/project1/elgita/women/admin.py
@@ -5,15 +5,22 @@ from .models import *
 
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
+    fields = (
+        'title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
 
     def get_html_photo(self, object):
-        return mark_safe(f'<img src="{object.photo.url}" width=50>')
+        if object.photo:
+            return mark_safe(f'<img src="{object.photo.url}" width=100>')
+
+    get_html_photo.short_description = 'Current photo'
 
 
 class CategoryAdmin(admin.ModelAdmin):
