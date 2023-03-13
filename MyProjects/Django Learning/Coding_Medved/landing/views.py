e871ef07fc2be763ab9c3cc98be19b147752d7cf
Dchamel <dchamel@yandex.ru>
2023-03-13 23:55:38 +0400

    - Coding Medved -- lesson 3

--- MyProjects/Django Learning/Coding_Medved/landing/views.py
+++ MyProjects/Django Learning/Coding_Medved/landing/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .forms import SubscriberForm
+
 
 def landing(request):
     name = 'CodingMedved'
+    form = SubscriberForm(request.POST or None)
     return render(request, 'landing/landing.html', locals())
