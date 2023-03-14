05d96c5eed33359c24a8eff2c20122db252abc37
Dchamel <dchamel@yandex.ru>
2023-03-14 18:56:32 +0400

    - Coding Medved -- lesson 3

--- MyProjects/Django Learning/Coding_Medved/landing/admin.py
+++ MyProjects/Django Learning/Coding_Medved/landing/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subscribers)
+
+class SubscriberAdmin(admin.ModelAdmin):
+    # list_display = ['name', 'email']
+    list_display = [field.name for field in Subscribers._meta.fields]
+    # list_filter = ['name']
+    search_fields = ['name']
+
+    # fields = ['name']
+    # exclude = ['name']
+
+    class Meta:
+        model = Subscribers
+
+
+admin.site.register(Subscribers, SubscriberAdmin)
--- MyProjects/Django Learning/Coding_Medved/landing/models.py
+++ MyProjects/Django Learning/Coding_Medved/landing/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Subscribers(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Id: %s, Login: %s' % (self.id, self.name)
+
+    class Meta:
+        verbose_name = 'Subscriber'
+        verbose_name_plural = 'Subscribers'
