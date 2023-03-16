aa784e0b6d0296bebfcf74eb6dcc74c9fa9f22de
Dchamel <dchamel@yandex.ru>
2023-03-16 23:00:36 +0400

    - Coding Medved -- lesson 5

---
+++ MyProjects/Django Learning/Coding_Medved/orders/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=50, blank=True, null=True, default=None)
+    customer_email = models.EmailField(blank=True, null=True, default=None)
+    customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return 'Order %s' % self.id
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
+class ProductInOrder(models.Model):
+    customer_name = models.CharField(max_length=50, blank=True, null=True, default=None)
+    customer_email = models.EmailField(blank=True, null=True, default=None)
+    customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return 'Order %s' % self.id
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
