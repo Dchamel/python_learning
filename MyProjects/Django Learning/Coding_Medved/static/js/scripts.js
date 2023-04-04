839b2694baf3c18afc78d447e3f3655c0b922458
Dchamel <dchamel@yandex.ru>
2023-04-04 21:01:09 +0400

    - Coding Medved -- lesson 10

--- MyProjects/Django Learning/Coding_Medved/static/js/scripts.js
+++ MyProjects/Django Learning/Coding_Medved/static/js/scripts.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
-    var form = $('#form_ordering_product');
-    console.log(form);
+    var form = $('#form_ordering_product')
+    console.log(form)
     form.on('submit', function (e) {
         e.preventDefault()
-        console.log('123');
-        var num = $('#number').val();
-        console.log(num);
+        console.log('123')
+        var num = $('#number').val()
+        console.log(num)
         var submit_btn = $('#submit_btn')
         var product_id = submit_btn.data('product-id')
         var product_name = submit_btn.data('product-name')
         var product_price = submit_btn.data('product-price')
-        console.log(product_id, product_name, product_price * num);
+        console.log(product_id, product_name, product_price * num)
+        $('.cart-items ul').append('<li>' + product_name + ' Quantity: ' + num + '</li>')
     })
+
+    function showingCart() {
+        $('.cart-items').toggleClass('hidden')
+    }
+
+    $('.cart-container').on('click', function (e) {
+        e.preventDefault()
+        showingCart()
+    })
+    $('.cart-container').mouseover(function () {
+        showingCart()
+    })
+
+    $('.cart-container').mouseout(function () {
+        showingCart()
+    })
+
 })
