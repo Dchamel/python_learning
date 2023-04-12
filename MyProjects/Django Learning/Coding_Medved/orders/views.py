468b8f81dec55c17a57e2d038ff8e9accd384386
Dchamel <dchamel@yandex.ru>
2023-04-12 23:39:08 +0400

    - Coding Medved -- lesson 13

--- MyProjects/Django Learning/Coding_Medved/orders/views.py
+++ MyProjects/Django Learning/Coding_Medved/orders/views.py
@@ -8,12 +8,16 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get('product_id')
     num = data.get('num')
+    is_delete = data.get('is_delete')
 
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
-                                                                 defaults={'number': num})
-    if not created:
-        new_product.number += int(num)
-        new_product.save(force_update=True)
+    if is_delete:
+        ProductInBasket.objects.filter(id=product_id).update(is_delete=False)
+    else:
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
+                                                                     defaults={'number': num})
+        if not created:
+            new_product.number += int(num)
+            new_product.save(force_update=True)
 
     products_in_cart = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     return_dict['products_total_numb'] = products_in_cart.count()
@@ -21,6 +25,7 @@ def basket_adding(request):
 
     for item in products_in_cart:
         products_dict = dict()
+        products_dict['id'] = item.id
         products_dict['name'] = item.product.name
         products_dict['price_per_item'] = item.price_per_item
         products_dict['num'] = item.number
--- MyProjects/Django Learning/Coding_Medved/static/js/scripts.js
+++ MyProjects/Django Learning/Coding_Medved/static/js/scripts.js
@@ -1,17 +1,8 @@
 $(document).ready(function () {
     var form = $('#form_ordering_product')
     console.log(form)
-    form.on('submit', function (e) {
-        e.preventDefault()
-        console.log('123')
-        var num = $('#number').val()
-        console.log(num)
-        var submit_btn = $('#submit_btn')
-        var product_id = submit_btn.data('product-id')
-        var product_name = submit_btn.data('product-name')
-        var product_price = submit_btn.data('product-price')
-        console.log(product_id, product_name, product_price * num)
 
+    function basketUpdating(product_id, num, is_delete) {
         var data = {}
         data.product_id = product_id
         data.num = num
@@ -34,7 +25,7 @@ $(document).ready(function () {
                     console.log(data.products)
                     $('.cart-items ul').html('')
                     $.each(data.products, function (k, v) {
-                        $('.cart-items ul').append('<li>' + v.name + '<br /> x ' + v.num + ' Total ' + v.price_per_item * num + ' mon</li>')
+                        $('.cart-items ul').append('<li>' + v.name + '<br /> x ' + v.num + ' Total: ' + v.price_per_item * num + ' mon <a class="delete-item" data-product_id="' + v.id + '" href="">x</a></li>')
                     })
                 }
             },
@@ -42,6 +33,20 @@ $(document).ready(function () {
                 console.log("error")
             }
         })
+    }
+
+    form.on('submit', function (e) {
+        e.preventDefault()
+        console.log('123')
+        var num = $('#number').val()
+        console.log(num)
+        var submit_btn = $('#submit_btn')
+        var product_id = submit_btn.data('product-id')
+        var product_name = submit_btn.data('product-name')
+        var product_price = submit_btn.data('product-price')
+        console.log(product_id, product_name, product_price * num)
+
+        basketUpdating(product_id, num, is_delete = false)
 
 
         $('.cart-items ul').append('<li>' + product_name + '<br /> x ' + num + ' Total ' + product_price * num + ' mon</li>')
@@ -65,7 +70,9 @@ $(document).ready(function () {
 
     $(document).on('click', '.delete-item', function (e) {
         e.preventDefault()
-        $(this).closest('li').remove()
+        product_id = $(this).data('product_id')
+        num = 0
+        basketUpdating(product_id, num, is_delete = true)
     })
 
 })
