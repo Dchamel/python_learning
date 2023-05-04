0ac15390dd3fc57e8ae0fcedccb89e764a51f821
Dchamel <dchamel@yandex.ru>
2023-05-04 23:55:08 +0400

    - Django_School -- lesson 5
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/models.py
+++ MyProjects/Django Learning/Django_School/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    '''Categories'''
+    name = models.CharField('Category', max_length=150)
+    description = models.TextField('Description')
+    url = models.SlugField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
