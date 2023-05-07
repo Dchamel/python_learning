4b28d76f2911486ba660bcb2ca916d6b00271cc1
Dchamel <dchamel@yandex.ru>
2023-05-07 23:50:19 +0400

    - Django_School -- lesson 6
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/models.py
+++ MyProjects/Django Learning/Django_School/movies/models.py
@@ -104,7 +104,7 @@ class Rating(models.Model):
     '''Rating'''
     ip = models.CharField('IP-Adress', max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Star')
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name='Movie')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Movie')
 
     def __str__(self):
         return f'{self.star} - {self.movie}'
