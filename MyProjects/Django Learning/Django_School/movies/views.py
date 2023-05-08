a5a77d79b7f6b636a8fb515505fb02af365dc4d5
Dchamel <dchamel@yandex.ru>
2023-05-08 23:58:06 +0400

    - Django_School -- lesson 7 (finding/fixing errors in templates) -- lesson 8
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/views.py
+++ MyProjects/Django Learning/Django_School/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movie_list.html', {'movie_list': movies})
