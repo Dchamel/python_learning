3fe5af1cfc024f6062051308b3ee2a74994421ba
Dchamel <dchamel@yandex.ru>
2023-05-09 23:58:40 +0400

    - Django_School -- lesson 8
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/Django_School/settings.py
+++ MyProjects/Django Learning/Django_School/Django_School/settings.py
@@ -112,6 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
