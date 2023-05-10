e831fbf6e32007cfb5b8c2bcd2030f308f164ef4
Dchamel <dchamel@yandex.ru>
2023-05-11 01:32:09 +0400

    - Django_School -- lesson 8
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/views.py
+++ MyProjects/Django Learning/Django_School/movies/views.py
@@ -7,4 +7,4 @@ from .models import Movie
 class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movies/movie_list.html', {'movie_list': movies})
+        return render(request, 'movies/movies.html', {'movie_list': movies})
