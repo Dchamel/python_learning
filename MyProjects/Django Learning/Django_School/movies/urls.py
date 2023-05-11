6e4338400023a21e07d8e9b7c91bc98b791a3e96
Dchamel <dchamel@yandex.ru>
2023-05-11 19:50:24 +0400

    - Django_School -- lesson 9
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/urls.py
+++ MyProjects/Django Learning/Django_School/movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MoviesView.as_view()),
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
 ]
--- MyProjects/Django Learning/Django_School/movies/views.py
+++ MyProjects/Django Learning/Django_School/movies/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
 
-class MoviesView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+class MoviesView(ListView):
+    '''All Films'''
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    '''One Film'''
+    model = Movie
+    slug_field = 'url'
+
+    # def get(self, request, slug):
+    #     movie = Movie.objects.get(url=slug)
+    #     return render(request, 'movies/movie_detail.html', {'movie': movie})
