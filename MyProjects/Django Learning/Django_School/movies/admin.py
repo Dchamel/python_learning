589b62ef7a84cadeba0a3c202936fdaad72a0783
Dchamel <dchamel@yandex.ru>
2023-05-16 23:29:23 +0400

    - Django_School -- lesson 13
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/admin.py
+++ MyProjects/Django Learning/Django_School/movies/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('email', 'name')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+
+    inlines = [ReviewInline]
+
+    save_on_top = True
+
+    save_as = True
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
