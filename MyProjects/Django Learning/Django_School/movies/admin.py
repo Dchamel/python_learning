07c5a1edb615144cae79403983386567005c92c6
Dchamel <dchamel@yandex.ru>
2023-05-18 19:13:01 +0400

    - Django_School -- lesson 13
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/admin.py
+++ MyProjects/Django Learning/Django_School/movies/admin.py
@@ -4,11 +4,13 @@ from .models import *
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    '''Categories'''
     list_display = ('id', 'name', 'url')
     list_display_links = ('name',)
 
 
 class ReviewInline(admin.TabularInline):
+    '''Reviews at the Film page'''
     model = Reviews
     extra = 1
     readonly_fields = ('email', 'name')
@@ -16,6 +18,7 @@ class ReviewInline(admin.TabularInline):
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    '''Films'''
     list_display = ('title', 'category', 'url', 'draft')
     list_filter = ('category', 'year')
     search_fields = ('title', 'category__name')
@@ -27,16 +30,56 @@ class MovieAdmin(admin.ModelAdmin):
     save_as = True
 
     list_editable = ('draft',)
+    # fields = (('actors', 'directors', 'genres'),)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'),)
+        }),
+        (None, {
+            'fields': ('description', 'poster')
+        }),
+        (None, {
+            'fields': (('year', 'country'),)
+        }),
+        ('Directors/Actors/Genres', {
+            'classes': ('collapse',),
+            'fields': ('actors', 'directors', 'genres', 'category')
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'),)
+        }),
+        ('Options', {
+            'fields': (('url', 'draft'),)
+        }),
+    )
 
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
+    '''Reviews'''
     list_display = ('name', 'email', 'parent', 'movie', 'id')
     readonly_fields = ('name', 'email')
 
 
-admin.site.register(Genre)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
-admin.site.register(RatingStar)
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    '''Genres'''
+    list_display = ('name', 'url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    '''Actors'''
+    list_display = ('name', 'age')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    '''Review'''
+    list_display = ('movie', 'star', 'ip')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    '''Shots from a Movie'''
+    list_display = ('title', 'movie')
