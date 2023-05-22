ed13dae5ba6902dec71a5b8449f18c109a3a4f04
Dchamel <dchamel@yandex.ru>
2023-05-23 02:38:31 +0400

    - Django_School -- lesson 17
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/views.py
+++ MyProjects/Django Learning/Django_School/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 
