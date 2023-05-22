c4d66d912604da8513781da7d1b6b4e57cb7e72c
Dchamel <dchamel@yandex.ru>
2023-05-22 23:25:04 +0400

    - Django_School -- lesson 16
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/admin.py
+++ MyProjects/Django Learning/Django_School/movies/admin.py
@@ -52,6 +52,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    actions = ['publish', 'unpublish']
     form = MovieAdminForm
     # fields = (('actors', 'directors', 'genres'),)
     fieldsets = (
@@ -82,6 +83,30 @@ class MovieAdmin(admin.ModelAdmin):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="120"')
 
     get_image.short_description = 'Image'
+
+    def unpublish(self, request, queryset):
+        '''Mass Unpublish'''
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 movie has been changed'
+        else:
+            message_bit = f'{row_update} movies had been changed'
+        self.message_user(request, f'{message_bit}')
+
+    def publish(self, request, queryset):
+        '''Mass Publish'''
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 movie has been changed'
+        else:
+            message_bit = f'{row_update} movies had been changed'
+        self.message_user(request, f'{message_bit}')
+
+    publish.short_description = 'Publish'
+    publish.allowed_permissions = ('change',)
+
+    unpublish.short_description = 'Unpublish'
+    unpublish.allowed_permissions = ('change',)
 
 
 @admin.register(Reviews)
