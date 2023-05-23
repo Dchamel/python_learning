37a95e9af21342383b2765d765a712e8ad789642
Dchamel <dchamel@yandex.ru>
2023-05-23 19:20:46 +0400

    - Django_School -- lesson 18
    still recovering after covid :(

--- MyProjects/Django Learning/Django_School/movies/models.py
+++ MyProjects/Django Learning/Django_School/movies/models.py
@@ -28,6 +28,9 @@ class Actor(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('actor_detail', kwargs={'slug': self.name})
+
     class Meta:
         verbose_name = 'Actors and Directors'
         verbose_name_plural = 'Actors and Directors'
