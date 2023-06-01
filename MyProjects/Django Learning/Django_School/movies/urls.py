e4e462beb41128129bc8419cfb4a68be5601c6dd
Dchamel <dchamel@yandex.ru>
2023-06-01 04:55:43 +0400

    - Django_School -- lesson 23

--- MyProjects/Django Learning/Django_School/movies/urls.py
+++ MyProjects/Django Learning/Django_School/movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.MoviesView.as_view()),
     # filter before slug(movie_detail)
+    path('search/', views.Search.as_view(), name='search'),
     path('filter/', views.FilterMoviesView.as_view(), name='filter'),
     path('add-rating/', views.AddStarRating.as_view(), name='add_rating'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
