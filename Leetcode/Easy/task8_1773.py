537f8a369e453026d17cecc2683e71a3d00a0e33
Dchamel <dchamel@yandex.ru>
2023-06-28 02:47:37 +0400

    https://leetcode.com/Dchamel/ leetcode  - task1773

--- Leetcode/Easy/task8_1773.py
+++ Leetcode/Easy/task8_1773.py
@@ -1,13 +1,17 @@
 import unittest
 from time import perf_counter
 
+import re
+
 t1 = perf_counter()
 
 
 def counter(items: list, ruleKey: str, ruleValue: str) -> int:
-    count_val = 0
-
-    return count_val
+    keys = ['type', 'color', 'name']
+    i = keys.index(ruleKey)
+    counter_val = len([item for item in items if re.search(rf'^{ruleValue}$', item[i])])
+    
+    return counter_val
 
 
 # tests
@@ -29,6 +33,13 @@ class AllTests(unittest.TestCase):
         actual = counter(
             items=[["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"]],
             ruleKey="type", ruleValue="phone")
+        self.assertEqual(expected, actual)
+
+    def test03_counter(self):
+        expected = 0
+        actual = counter(
+            items=[["ii", "iiiiiii", "ii"], ["iiiiiii", "iiiiiii", "ii"], ["ii", "iiiiiii", "iiiiiii"]],
+            ruleKey="color", ruleValue="ii")
         self.assertEqual(expected, actual)
 
 
