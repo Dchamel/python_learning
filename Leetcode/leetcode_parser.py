014de68b4bc140da1bd21edfbe288ebfe7ff342a
Dchamel <dchamel@yandex.ru>
2023-08-08 21:04:59 +0400

    https://leetcode.com/Dchamel/ leetcode  - Parser for LeetCode tasks v0.20

--- Leetcode/leetcode_parser.py
+++ Leetcode/leetcode_parser.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import codecs
 import html
+import re
 
 # URL = 'https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/'
 URL = 'https://leetcode.com/problems/sort-the-people'
@@ -45,33 +46,69 @@ def parse_html(html):
 with open("tmp.html", "r", encoding='utf-8') as f:
     html_data = f.read()
 
-    soup = BeautifulSoup(html_data, "lxml")
-    script = soup.find('script', {'id': '__NEXT_DATA__'}).text
-    data = json.loads(script)
-    # data = json.dumps(data, indent=4, sort_keys=True)
-    # with open('tmp.json', 'w') as f:
-    # json.dump(data, f)
-    task_num = data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['question'][
-        'questionFrontendId']
-    task_title = data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['question'][
-        'title']
-    task_content = data['props']['pageProps']['dehydratedState']['queries'][6]['state']['data']['question']['content']
+soup = BeautifulSoup(html_data, "lxml")
+script = soup.find('script', {'id': '__NEXT_DATA__'}).text
+data = json.loads(script)
+# data = json.dumps(data, indent=4, sort_keys=True)
+# with open('tmp.json', 'w') as f:
+# json.dump(data, f)
+
+task_num = data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['question'][
+    'questionFrontendId']
+task_title = data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['question'][
+    'title']
+task_content = data['props']['pageProps']['dehydratedState']['queries'][6]['state']['data']['question']['content']
+
+
+def main_text_split(task_content: str) -> str:
+    main_text_finished = ''
     task_content_unescape = html.unescape(task_content)
     task_content_unescape = task_content_unescape.replace('<code>', '<')
     task_content_unescape = task_content_unescape.replace('</code>', '>')
-    elem2 = BeautifulSoup(task_content_unescape, features="html.parser")
-    q = elem2.find_all('pre')
-    q1 = q[0].text
-    example_input = q1.split('\n')
-    print(example_input[1].replace('Input: ', ''))
-    print(example_input[2].replace('Output: ', ''))
-    print(example_input[3].replace('Explanation: ', ''))
+    elem = BeautifulSoup(task_content_unescape, features="html.parser")
+    examples = elem.find_all('pre')
+    # print(examples)
+    for s in elem.select('pre'):
+        s.extract()
+    print(elem)
 
-    # q = parse_html(task_content_unescape)
-    # print(task_num)
-    # print(task_title)
-    # print(task_content_unescape)
-    # print(q)
+    # Preparing all examples for split
+    raw_examples_list = []
+    for raw_example in examples:
+        raw_examples_list.append(raw_example.text)
+    # print(raw_examples_list)
+
+    # Splitting Examples and putting them to final list
+    examples_list_splitted = []
+    for raw_example in raw_examples_list:
+        examples_list_splitted.append(raw_example.split('\n'))
+    examples_list_4_vars = []
+    for example in examples_list_splitted:
+        example_str_list = [i for i in example if i]
+        example_str_list2 = []
+        for string in example_str_list:
+            match string:
+                case string if string.startswith('Input: '):
+                    example_str_list2.append(string.replace('Input: ', ''))
+                case string if string.startswith('Output: '):
+                    example_str_list2.append(string.replace('Output: ', ''))
+        examples_list_4_vars.append(example_str_list2)
+    # print(examples_list_4_vars)
+    main_text_finished = task_content_unescape
+    # print(main_text_finished)
+    return main_text_finished
+
+
+main_text_split(task_content)
+# print(example_input[1].replace('Input: ', ''))
+# print(example_input[2].replace('Output: ', ''))
+# print(example_input[3].replace('Explanation: ', ''))
+
+# q = parse_html(task_content_unescape)
+# print(task_num)
+# print(task_title)
+# print(task_content_unescape)
+# print(q)
 
 # soup = BeautifulSoup(html, "html.parser")
 # script = soup.find('script', {'id': '__NEXT_DATA__'})
