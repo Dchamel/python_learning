06223390a428201c34448bafbfe3a0e66609181c
Dchamel <dchamel@yandex.ru>
2023-09-01 17:30:01 +0400

    Did Coding  - Module 1,2,3

---
+++ MyProjects/Django Learning/Did_Coding/core/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+
+
+class HomeView(generic.TemplateView):
+    """
+    Website home page.
+
+    **Template:**
+
+    :template:`core/index.html`
+    """
+    template_name = "core/index.html"
