c61901016aead168d9ee3d8228312a8cdc6fe0e0
Dchamel <dchamel@yandex.ru>
2023-09-25 23:44:05 +0400

    Alex_Medovnik  - lesson 1,2,3,4,5,6,7,8

---
+++ MyProjects/Django Learning/Alex_Medovnik/Alex_Medovnik/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Alex_Medovnik project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alex_Medovnik.settings')
+
+application = get_wsgi_application()
