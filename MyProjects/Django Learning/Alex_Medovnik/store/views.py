b003d19a2761129c76c6407891350cd129618886
Dchamel <dchamel@yandex.ru>
2023-09-27 23:40:55 +0400

    Alex_Medovnik  - lesson 11, 12

--- MyProjects/Django Learning/Alex_Medovnik/store/views.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def build_template(lst: list, cols: int) -> list:
+    new_list = []
+    for i in range(0, len(lst), cols):
+        new_list.append(lst[i:i + cols])
+    return new_list
+
+
+print(build_template([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+
+
 def product_list(request):
-    return render(request, 'store/product_list.html', context={'product_title': 'Range Rover'})
+    products = [
+        {'title': 'Range Rover', 'Info': 'lorem ipsum...', 'price': 10000},
+        {'title': 'Land Rover', 'Info': 'lorem ipsum...', 'price': 8000},
+        {'title': 'Range Rover Sport', 'Info': 'lorem ipsum...', 'price': 12000},
+    ]
+
+    return render(request, 'store/product_list.html',
+                  context={'products': products})
