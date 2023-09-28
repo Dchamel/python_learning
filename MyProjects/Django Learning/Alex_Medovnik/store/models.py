2d5b473f8719057b3277bbd3f5ff03f465eba9aa
Dchamel <dchamel@yandex.ru>
2023-09-28 16:14:02 +0400

    Alex_Medovnik  - lesson 13

--- MyProjects/Django Learning/Alex_Medovnik/store/models.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    info = models.TextField(blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- MyProjects/Django Learning/Alex_Medovnik/store/views.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 
 def build_template(lst: list, cols: int) -> list:
-    new_list = []
-    for i in range(0, len(lst), cols):
-        new_list.append(lst[i:i + cols])
-    return new_list
-
-
-print(build_template([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+    return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
-    products = [
-        {'title': 'Range Rover', 'Info': 'lorem ipsum...', 'price': 10000},
-        {'title': 'Land Rover', 'Info': 'lorem ipsum...', 'price': 8000},
-        {'title': 'Range Rover Sport', 'Info': 'lorem ipsum...', 'price': 12000},
-    ]
+    products = Product.objects.all()
 
     return render(request, 'store/product_list.html',
-                  context={'products': products})
+                  context={'product_list': build_template(products, 2)})
