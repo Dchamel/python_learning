a323f6551c79ca4b65cb600f531a8260da6a6638
Dchamel <dchamel@yandex.ru>
2023-10-05 16:14:47 +0400

    Alex_Medovnik  - lesson 24,25

--- MyProjects/Django Learning/Alex_Medovnik/store/urls.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import product_list, product_detail, category_detail, save_order
+from .views import HomeView, ProductView, CategoryView, save_order
 
 urlpatterns = [
-    path('', product_list, name='products'),
-    path('product/<int:pk>/', product_detail, name='product_detail'),
-    path('category/<int:pk>/', category_detail, name='category_detail'),
+    path('', HomeView.as_view(), name='products'),
+    path('product/<int:pk>/', ProductView.as_view(), name='product_detail'),
+    path('category/<int:pk>/', CategoryView.as_view(), name='category_detail'),
     path('save_order', save_order),
 ]
--- MyProjects/Django Learning/Alex_Medovnik/store/views.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/views.py
@@ -1,49 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
 from .models import *
 
 
-def build_template(lst: list, cols: int) -> list:
-    return [lst[i:i + cols] for i in range(0, len(lst), cols)]
+# def build_template(lst: list, cols: int) -> list:
+#     return [lst[i:i + cols] for i in range(0, len(lst), cols)]
+
+class HomeView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('search', None)
+        if search_query:
+            return self.model.objects.filter(
+                Q(title__icontains=search_query) | Q(info__icontains=search_query)
+            )
+        return self.model.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
-def product_list(request):
-    categories = Category.objects.all()
-    search_query = request.GET.get('search', None)
-    if search_query:
-        product_list = Product.objects.filter(
-            Q(title__icontains=search_query) | Q(info__icontains=search_query)
-        )
-    else:
-        product_list = Product.objects.all()
+class ProductView(DetailView):
+    model = Product
 
-    return render(request, 'store/product_list.html', context={
-        'product_list': product_list,
-        'categories': categories,
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
-def product_detail(request, pk):
-    categories = Category.objects.all()
-    product = Product.objects.get(pk=pk)
-    return render(request, 'store/product_detail.html', context={
-        'categories': categories,
-        'product': product,
-    })
+class CategoryView(DetailView):
+    model = Category
 
-
-def category_detail(request, pk):
-    categories = Category.objects.all()
-    category = Category.objects.get(pk=pk)
-    product_list = category.products.all()
-
-    return render(request, 'store/category_detail.html', context={
-        'product_list': product_list,
-        'categories': categories,
-        'category': category,
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 def save_order(request):
