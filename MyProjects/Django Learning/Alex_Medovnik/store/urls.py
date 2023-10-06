3b680ea081e7906ebef76cb4490fe6ebc4919901
Dchamel <dchamel@yandex.ru>
2023-10-06 21:08:33 +0400

    Alex_Medovnik  - lesson 27

--- MyProjects/Django Learning/Alex_Medovnik/store/urls.py
+++ MyProjects/Django Learning/Alex_Medovnik/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ProductView, CategoryView, save_order
+from .views import HomeView, ProductView, CategoryView, save_order, work
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='products'),
+    # path('', HomeView.as_view(), name='products'),
+    path('', work),
     path('product/<int:pk>/', ProductView.as_view(), name='product_detail'),
     path('category/<int:pk>/', CategoryView.as_view(), name='category_detail'),
     path('save_order', save_order),
