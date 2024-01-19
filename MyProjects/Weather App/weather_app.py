1518c12e87314f4481a941eee4c664cfb5449fc0
Dchamel <dchamel@yandex.ru>
2024-01-19 23:47:32 +0400

    Small Weather App  - preparing for API

--- MyProjects/Weather App/weather_app.py
+++ MyProjects/Weather App/weather_app.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 from time import perf_counter
-from aiohttp import ClientSession
+from aiohttp import ClientSession, web
 
 t1 = perf_counter()
 
@@ -17,24 +17,48 @@ async def get_weather(city):
 
         async with session.get(url=url, params=params) as response:
             weather_json = await response.json()
-            print(f'{city}: {weather_json["weather"][0]["main"]}({weather_json["weather"][0]["description"]})')
-            print(
-                f'Temperature: {weather_json["main"]["temp"]:.1f}\N{DEGREE SIGN}c Feels Like: {weather_json["main"]["feels_like"]:.1f}\N{DEGREE SIGN}c\n')
+            try:
+                city_main = f'{city}: {weather_json["weather"][0]["main"]}({weather_json["weather"][0]["description"]})'
+                city_temp = f'Temperature: {weather_json["main"]["temp"]:.1f}\N{DEGREE SIGN}c Feels Like: {weather_json["main"]["feels_like"]:.1f}\N{DEGREE SIGN}c\n'
+                city_weather = [city_main, city_temp]
+                return city_weather
+            except KeyError:
+                'No data'
             # print(json.dumps(weather_json, indent=4))
 
 
-async def main(cities_):
-    tasks = []
-    for city in cities_:
-        tasks.append(asyncio.create_task(get_weather(city)))
-
-    for task in tasks:
-        await task
+async def handle(request):
+    city = request.rel_url.query['city']
+    result = await get_weather(city)
+    return result
 
 
-cities = ['Samara', 'Pskov', 'Moscow', 'St. Petersburg', 'Szczecin']
-# cities = ['Samara']
-asyncio.run(main(cities))
+async def main():
+    app = web.Application()
+    app.add_routes([web.get('/weather', handle)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 8000)
+    await site.start()
+
+    while True:
+        await asyncio.sleep(3000)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+    # tasks = []
+
+    # for city in cities_:
+    #     tasks.append(asyncio.create_task(get_weather(city)))
+    #
+    # for task in tasks:
+    #     await task
+
+# cities = ['Samara', 'Pskov', 'Moscow', 'St. Petersburg', 'Szczecin']
+# # cities = ['Samara']
+# asyncio.run(main(cities))
 
 t2 = perf_counter()
 print(f'Working time: {t2 - t1:.5f} sec')
