696c2b261a917513e10db4bd6e89673f45c29326
Dchamel <dchamel@yandex.ru>
2024-02-03 22:35:44 +0400

    Still working at logger aiologger not compatible with Windows

--- MyProjects/Weather App/weather_app.py
+++ MyProjects/Weather App/weather_app.py
@@ -4,23 +4,19 @@ import asyncio
 # from dotenv import load_dotenv
 from time import perf_counter
 from aiohttp import ClientSession, web
-from aiologger.loggers.json import JsonLogger
 
 from googletrans import Translator
 
 t1 = perf_counter()
 
+
 # load_dotenv()
 # yapi_key = os.environ['GOOGLE_API_KEY']
-
-logger = JsonLogger.with_default_handlers(
-    level='DEBUG',
-    serializer_kwargs={'ensure_ascii': False},
-)
 
 
 async def get_weather(city: str) -> list:
     async with ClientSession() as session:
+
         url = f'http://api.openweathermap.org/data/2.5/weather'
         params = {
             'q': city,
@@ -29,29 +25,24 @@ async def get_weather(city: str) -> list:
         }
 
         async with session.get(url=url, params=params) as response:
-            await logger.info(f'Sending request with city to Weather server: {city}')
             weather_json = await response.json()
             try:
                 city_main = f'{city}: {weather_json["weather"][0]["main"]}({weather_json["weather"][0]["description"]})'
                 city_temp = f'Temperature: {weather_json["main"]["temp"]:.1f}\N{DEGREE SIGN}c Feels Like: {weather_json["main"]["feels_like"]:.1f}\N{DEGREE SIGN}c\n'
                 city_weather = [city_main, city_temp]
-                await logger.info(f'Data has been received for city: {city}')
                 return city_weather
             except KeyError:
-                await logger.info(f'Data receive FAILURE: {city}')
                 return 'No data'
             # print(json.dumps(weather_json, indent=4))
 
 
 # Finally make it works but only with googletrans==3.1.0a0
 async def get_translation(text, main='en', dest='ru'):
-    await logger.info(f'Sending text to translate: {text}')
     translator = Translator()
     try:
         translated_list = translator.translate(text, src=main, dest=dest)
         translated_text = '\n'.join([i.text for i in translated_list])
     except KeyError:
-        logger.error(f"Can't translate: {text}")
         return text
 
     return translated_text
@@ -59,8 +50,6 @@ async def get_translation(text, main='en', dest='ru'):
 
 async def handle(request):
     city = request.rel_url.query['city']
-    await logger.info(f'Received Request for the city: {city}')
-
     weather_en = await get_weather(city)
     weather_ru = await get_translation(weather_en, 'en', 'ru')
     # weather_json = json.dumps(weather_ru, ensure_ascii=False)
