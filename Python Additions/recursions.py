cec25dd4974ca4d5d85634f11af7e2892c3d79b0
Dchamel <dchamel@yandex.ru>
2024-03-04 20:47:05 +0400

    - Learning Recursions

--- Python Additions/recursions.py
+++ Python Additions/recursions.py
@@ -1,6 +1,8 @@
 from time import perf_counter
 
 t1 = perf_counter()
+
+
 # ------BOF------
 
 
@@ -121,9 +123,12 @@ t1 = perf_counter()
 # print(recurs_factor7('pineapple'))
 
 
-# task 8 - double letters ------------------- UNFINISHED
+# task 8 - double letters
 # def recurs_factor8(n: str) -> str:
 #     """Takes a str, doubles each letter in it and return this new str"""
+#     if not n:
+#         return n
+#     return n[0] * 2 + recurs_factor8(n[1:])
 #
 #
 # print(recurs_factor8('apple'))
@@ -156,23 +161,30 @@ t1 = perf_counter()
 
 # task 8.2 from Ann ^_^
 # Square all elements of the list
-list_data = [1, [2, 7, 8], [3, 4], [5, [6, 7]]]
+# list_data = [1, [2, 7, 8], [3, 4], [5, [6, 7]]]
+#
+#
+# def recurs_82(list_data: list[int | list]) -> list[int | list]:
+#     """
+#     Recursion. Takes elements of the list one by one and Square them.
+#     """
+#     for i, each in enumerate(list_data):
+#         if type(each) == int:
+#             list_data[i] = list_data[i] ** 2
+#         else:
+#             recurs_82(each)
+#
+#     return list_data
+#
+#
+# print(recurs_82(list_data))
+
+# task 9 - Pascal triangle
+def recurs_factor9(n: int) -> str:
+    """111"""
 
 
-def recurs_82(list_data: list[int | list]) -> list[int | list]:
-    """
-    Recursion. Takes elements of the list one by one and Square them.
-    """
-    for i, each in enumerate(list_data):
-        if type(each) == int:
-            list_data[i] = list_data[i] ** 2
-        else:
-            recurs_82(each)
-
-    return list_data
-
-
-print(recurs_82(list_data))
+print(recurs_factor9('apple'))
 
 # ------EOF------
 t2 = perf_counter()
