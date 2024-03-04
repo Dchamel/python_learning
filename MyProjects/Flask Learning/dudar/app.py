c1155331605b48e38d987cb3b3734865fa2856c3
Dchamel <dchamel@yandex.ru>
2024-03-04 08:26:13 +0400

    Flask Learning - SQL Alchemy

--- MyProjects/Flask Learning/dudar/app.py
+++ MyProjects/Flask Learning/dudar/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daduda.db'
@@ -11,7 +12,10 @@ class Article(db.Model):
     title = db.Column(db.String(100), nullable=False)
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
