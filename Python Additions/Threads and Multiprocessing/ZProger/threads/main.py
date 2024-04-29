1be52ad087648bbbe91ab1ba511d1f14abb890d6
Dchamel <dchamel@yandex.ru>
2024-04-29 22:55:07 +0400

    ZProger  - Lesson1  - Thereading

--- Python Additions/Threads and Multiprocessing/ZProger/threads/main.py
+++ Python Additions/Threads and Multiprocessing/ZProger/threads/main.py
@@ -2,36 +2,18 @@ import threading
 import time
 
 
-def get_data(data):
-    while True:
+def get_data(data, val):
+    for _ in range(val):
         print(f'{threading.current_thread().name} - {data}')
         time.sleep(1)
 
 
-thr = threading.Thread(target=get_data, args=(str(time.time()),), name='thr-1')
-thr.start()
+thr_list = []
 
-for _ in range(20):
-    print(f'current {_}')
-    time.sleep(1)
+for i in range(3):
+    thr = threading.Thread(target=get_data, args=(str(time.time()), i,), name=f'thr-{i}')
+    thr_list.append(thr)
+    thr.start()
 
-    if _ % 10 == 0:
-        print('active thread:', threading.active_count())
-        print('enumerate:', threading.enumerate())
-        print('thr-1 is alive', thr.is_alive())
-        print(f'main thread: {threading.main_thread().name}')
-
-print('name:', threading.main_thread().name)
-threading.main_thread().name = 'абырвалГ'
-# threading.main_thread().name = 'qwe123'
-print('result:', threading.main_thread().name)
-
-for _ in range(20):
-    print(f'current {_}')
-    time.sleep(1)
-
-    if _ % 10 == 0:
-        print('active thread:', threading.active_count())
-        print('enumerate:', threading.enumerate())
-        print('thr-1 is alive', thr.is_alive())
-        print(f'main thread: {threading.main_thread().name}')
+for i in thr_list:
+    i.join()
