1b3b5299d9251a3d064a6cd7de9d5f4d152aab79
Dchamel <dchamel@yandex.ru>
2024-06-04 23:51:35 +0400

    ZProger  - Lesson7  - Multiprocessing

--- Python Additions/Threads and Multiprocessing/ZProger/threads/lesson7/main.py
+++ Python Additions/Threads and Multiprocessing/ZProger/threads/lesson7/main.py
@@ -60,17 +60,28 @@ import time
 
 def add_value(locker, array, index):
     with locker:
-        num = random.randint(0, 20)
+        num = random.randint(0, 3)
         vtime = time.ctime()
         array[index] = num
         print(f'array[{index}] = {num}, time = [{vtime}]')
         time.sleep(num)
+        print(f'array[{index}] finished')
 
 
-lock = multiprocessing.Lock()
-arr = multiprocessing.Array('i', range(10))
-processes = []
+if __name__ == '__main__':
 
-for i in range(10):
-    pr = multiprocessing.Process(target=add_value, args=(lock, arr, i)).start()
-    processes.append(pr)
+    lock = multiprocessing.Lock()
+    arr = multiprocessing.Array('i', range(10))
+    processes = []
+
+    for i in range(10):
+        pr = multiprocessing.Process(target=add_value, args=(lock, arr, i))
+        processes.append(pr)
+
+    for p in processes:
+        p.start()
+
+    # for p in processes:
+    #     p.join()
+
+    print(list(arr))
