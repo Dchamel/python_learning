61f03b8b442d29fc45355746dd5740f3793591e5
dchamel <dchamel@yandex.ru>
2024-07-30 23:56:35 +0400

    SB - Patterns  - Behavioral - Observer

--- Python Additions/Patterns/behavioral.py
+++ Python Additions/Patterns/behavioral.py
@@ -5,90 +5,89 @@ from abc import ABC, abstractmethod
 
 
 # Pattern Strategy
-# class Strategy(ABC):
-#     """Abstract base class for Strategies."""
-#
-#     @abstractmethod
-#     def calc_effective_path(self, deata):
-#         pass
-#
-#
-# class AutoStrategy(Strategy):
-#     """Strategy for delivery by car"""
-#
-#     def calc_effective_path(self, data):
-#         # calc path
-#         return data
-#
-#
-# class BikeStrategy(Strategy):
-#     """Strategy for delivery by bike"""
-#
-#     def calc_effective_path(self, data):
-#         # calc path
-#         return data
-#
-#
-# class Courier:
-#     def __init__(self, strategy: Strategy) -> None:
-#         """Takes a strategy as a parameter"""
-#         self._strategy = strategy
-#
-#     @property
-#     def strategy(self) -> Strategy:
-#         """Access from code"""
-#         return self._strategy
-#
-#     @strategy.setter
-#     def strategy(self, strategy: Strategy) -> None:
-#         """Set up new strategy"""
-#         self._strategy = strategy
-#
-#     def get_effective_path(self, data) -> None:
-#         """Calculate effective path"""
-#         return self._strategy.calc_effective_path(data)
+class Strategy(ABC):
+    """Abstract base class for Strategies."""
 
+    @abstractmethod
+    def calc_effective_path(self, deata):
+        pass
+
+
+class AutoStrategy(Strategy):
+    """Strategy for delivery by car"""
+
+    def calc_effective_path(self, data):
+        # calc path
+        return data
+
+
+class BikeStrategy(Strategy):
+    """Strategy for delivery by bike"""
+
+    def calc_effective_path(self, data):
+        # calc path
+        return data
+
+
+class Courier:
+    def __init__(self, strategy: Strategy) -> None:
+        """Takes a strategy as a parameter"""
+        self._strategy = strategy
+
+    @property
+    def strategy(self) -> Strategy:
+        """Access from code"""
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: Strategy) -> None:
+        """Set up new strategy"""
+        self._strategy = strategy
+
+    def get_effective_path(self, data) -> None:
+        """Calculate effective path"""
+        return self._strategy.calc_effective_path(data)
 
 # Pattern Observer
 
-class ShopList():
-    """Class for storing"""
-
-    def __init__(self):
-        # list of the clients
-        self._observers = []
-
-    def attach(self, observer) -> None:
-        self._observers.append(observer)
-
-    def detach(self, observer) -> None:
-        self._observers.remove(observer)
-
-    def notify(self) -> None:
-        """Notify all clients"""
-        for observer in self._observers:
-            observer.update(self)
-
-    def business_logic(self) -> None:
-        """SOme business logic"""
-        self.notify()
-
-
-class Observer(ABC):
-    """Abstract Observer class"""
-
-    @abstractmethod
-    def update(self, shop_list_object) -> None:
-        pass
-
-
-class EmailObserver(Observer):
-    def update(self, shop_list_object) -> None:
-        pass
-
-
-class SmsObserver(Observer):
-    def update(self, shop_list_object) -> None:
-        pass
-
-# Pattern
+# class ShopList():
+#     """Class for storing"""
+#
+#     def __init__(self):
+#         # list of the clients
+#         self._observers = []
+#
+#     def attach(self, observer) -> None:
+#         self._observers.append(observer)
+#
+#     def detach(self, observer) -> None:
+#         self._observers.remove(observer)
+#
+#     def notify(self) -> None:
+#         """Notify all clients"""
+#         for observer in self._observers:
+#             observer.update(self)
+#
+#     def business_logic(self) -> None:
+#         """SOme business logic"""
+#         self.notify()
+#
+#
+# class Observer(ABC):
+#     """Abstract Observer class"""
+#
+#     @abstractmethod
+#     def update(self, shop_list_object) -> None:
+#         pass
+#
+#
+# class EmailObserver(Observer):
+#     def update(self, shop_list_object) -> None:
+#         pass
+#
+#
+# class SmsObserver(Observer):
+#     def update(self, shop_list_object) -> None:
+#         pass
+#
+# # Pattern
