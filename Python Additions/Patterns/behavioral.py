567f5897f9ce0a35fe7ebafb54912fab46e29ad7
dchamel <dchamel@yandex.ru>
2024-08-01 11:13:13 +0400

    SB - Patterns  - Behavioral - State

--- Python Additions/Patterns/behavioral.py
+++ Python Additions/Patterns/behavioral.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+
 
 # Behavioral Patterns from SB
 
@@ -96,41 +98,87 @@ from abc import ABC, abstractmethod
 
 # # Pattern Command
 
-from __future__ import annotations
-from abc import ABC, abstractmethod
+# class Command(ABC):
+#     """Abstract base class for Command"""
+#
+#     @abstractmethod
+#     def execute(self):
+#         pass
+#
+#
+# class PrintCommand(Command):
+#     def execute(self):
+#         print("wohoo")
+#
+#
+# class CreateFileCommand(Command):
+#     def __init__(self, file_name):
+#         self._file_name = file_name
+#
+#     def execute(self) -> None:
+#         f = open(self._file_name, "w+")
+#         f.close()
+#
+#
+# class CommandManager:
+#     """Command initialization"""
+#
+#     def start_command(self, command: Command):
+#         self._start_command = command
+#
+#     def stop_command(self, command: Command):
+#         self._stop_command = command
+#
+#     def do_something(self) -> None:
+#         """Do something useful"""
+#         pass
+
+# # -----------------------------------------------------------
+
+# # Pattern State
+
+class ContextHandler(ABC):
+    """Class representing a business-logic of the app and stores links at states"""
+
+    def __init__(self, state):
+        self._state = state
+
+    def set_state(self, state: State):
+        self._state = state
+        self._state.context = self
+
+    def send_request(self):
+        self._state.handle()
 
 
-class Command(ABC):
-    """Abstract base class for Command"""
+class State(ABC):
+    """Base state class"""
+
+    @property
+    def context(self):
+        return self._context
+
+    @context.setter
+    def context(self, context):
+        self._context = context
 
     @abstractmethod
-    def execute(self):
+    def handle(self) -> None:
         pass
 
 
-class PrintCommand(Command):
-    def execute(self):
-        print("wohoo")
-
-
-class CreateFileCommand(Command):
-    def __init__(self, file_name):
-        self._file_name = file_name
-
-    def execute(self) -> None:
-        f = open(self._file_name, "w+")
-        f.close()
-
-
-class CommandManager:
-    """Command initialization"""
-
-    def start_command(self, command: Command):
-        self._start_command = command
-
-    def stop_command(self, command: Command):
-        self._stop_command = command
-
-    def do_something(self) -> None:
-        """Do something useful"""
+class SmsState(State):
+    def send_sms(self, text: str) -> None:
         pass
+
+    def handle(self) -> None:
+        self.send_sms("test")
+
+
+class PushState(State):
+
+    def send_push(self, text):
+        pass
+
+    def handle(self) -> None:
+        self.send_push("test")
