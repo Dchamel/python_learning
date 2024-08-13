3fbe51f98970d0d9f606d60821cbeb69df2fda0a
dchamel <dchamel@yandex.ru>
2024-08-13 16:15:25 +0400

    SB - Patterns  - Structural - Facade

--- Python Additions/Patterns/structural.py
+++ Python Additions/Patterns/structural.py
@@ -1,12 +1,43 @@
 import time
 
 
-def func_time_decorator(function):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        return_value = function(*args, **kwargs)
-        end = time.time()
-        print('Function took {} seconds'.format(end - start))
-        return return_value
+# def func_time_decorator(function):
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         return_value = function(*args, **kwargs)
+#         end = time.time()
+#         print('Function took {} seconds'.format(end - start))
+#         return return_value
+#
+#     return wrapper
 
-    return wrapper
+class FileAdapterFacade:
+    """Facade for accessing files that hides system where file is located."""
+
+    def __init__(self, subsystem):
+        self._file_system = subsystem
+
+    def get_file(self, file_name):
+        return self._file_system.get(file_name)
+
+
+class LocalStorager:
+    """Access to local files."""
+
+    def get_file(self, file_name):
+        pass
+
+
+class CephStorager:
+    """
+    Access to remote files at ceph bucket
+    Class for implementing Ceph storage
+    """
+
+    def get_file(self, file_name):
+        connector = self.connect()
+        connector.set_s3_mode()
+        connector.turn_off_sll()
+        connector.establish()
+        file = connector.get_file(file_name)
+        return file
