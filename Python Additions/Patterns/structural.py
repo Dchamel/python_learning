60dc7ded2db9018fe421c053407d258dc3826819
dchamel <dchamel@yandex.ru>
2024-08-14 14:31:20 +0400

    SB - Patterns  - Structural - Adapter

--- Python Additions/Patterns/structural.py
+++ Python Additions/Patterns/structural.py
@@ -1,5 +1,4 @@
-import time
-
+import time, requests
 
 # Pattern Decorator
 
@@ -14,33 +13,80 @@ import time
 #     return wrapper
 
 # Pattern Facade
-class FileAdapterFacade:
-    """Facade for accessing files that hides system where file is located."""
 
-    def __init__(self, subsystem):
-        self._file_system = subsystem
+# class FileAdapterFacade:
+#     """Facade for accessing files that hides system where file is located."""
+#
+#     def __init__(self, subsystem):
+#         self._file_system = subsystem
+#
+#     def get_file(self, file_name):
+#         return self._file_system.get(file_name)
+#
+#
+# class LocalStorager:
+#     """Access to local files."""
+#
+#     def get_file(self, file_name):
+#         pass
+#
+#
+# class CephStorager:
+#     """
+#     Access to remote files at ceph bucket
+#     Class for implementing Ceph storage
+#     """
+#
+#     def get_file(self, file_name):
+#         connector = self.connect()
+#         connector.set_s3_mode()
+#         connector.turn_off_sll()
+#         connector.establish()
+#         file = connector.get_file(file_name)
+#         return file
 
-    def get_file(self, file_name):
-        return self._file_system.get(file_name)
+# Pattern Adapter
+
+CORRELATION_ID = 'correlation_id'
 
 
-class LocalStorager:
-    """Access to local files."""
+class HttpRequests:
+    def __init__(self, url):
+        self.url = url
 
-    def get_file(self, file_name):
-        pass
+    def request(self, obj):
+        try:
+            if CORRELATION_ID not in obj.headers.keys():
+                raise AttributeError
+            else:
+                response = requests.get(self.url, params=obj.params, headers=obj.headers)
+        except AttributeError:
+            print("not correlation_id in headers")
 
 
-class CephStorager:
-    """
-    Access to remote files at ceph bucket
-    Class for implementing Ceph storage
-    """
+class OldClass:
+    def __init__(self, headers, params):
+        self.headers = headers
+        self.params = params
 
-    def get_file(self, file_name):
-        connector = self.connect()
-        connector.set_s3_mode()
-        connector.turn_off_sll()
-        connector.establish()
-        file = connector.get_file(file_name)
-        return file
+
+class OldClassHttpRequestAdapter:
+    def __init__(self, obj):
+        self.obj = obj
+
+    @property
+    def headers(self):
+        if CORRELATION_ID not in self.obj.headers.keys():
+            self.obj.headers[CORRELATION_ID] = '1232-1112-3333'
+        return self.obj.headers
+
+    @property
+    def params(self):
+        return self.obj.params
+
+
+http_requests = HttpRequests('htpps://github.com')
+old_class_obj = OldClass({'test': '111'}, {'params1': 'value1'})
+adapter_obj = OldClassHttpRequestAdapter(old_class_obj)
+http_requests.request(old_class_obj)
+http_requests.request(adapter_obj)
