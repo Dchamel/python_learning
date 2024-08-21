274cbba0e757e48af37f29af82881a8e7b488bce
dchamel <dchamel@yandex.ru>
2024-08-21 11:14:43 +0400

    SB - Data structures and additions

--- Python Additions/Build-In modules/data_struct_and_additions.py
+++ Python Additions/Build-In modules/data_struct_and_additions.py
@@ -12,25 +12,110 @@
 #
 # # -----------------------------
 
-# bisect
-# insert to ordered list
-import bisect
+# # bisect
+# # insert to ordered list
+# import bisect
+#
+# # example1
+# ordered_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(bisect.bisect_left(ordered_list, 2))
+# print(bisect.bisect_right(ordered_list, 2))
+# print(ordered_list)
+# print(bisect.insort_left(ordered_list, 2))
+# print(ordered_list)
+#
+#
+# # example2
+# def grade(score, breakpoints=[30, 40, 50, 60], grades='ABC'):
+#     i = bisect(breakpoints, score)
+#     return grades[i]
+#
+#
+# print([grade(score) for score in [32, 43, 11, 40, 20, 200]])
+#
+# # -----------------------------
 
-# example1
-ordered_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(bisect.bisect_left(ordered_list, 2))
-print(bisect.bisect_right(ordered_list, 2))
-print(ordered_list)
-print(bisect.insort_left(ordered_list, 2))
-print(ordered_list)
+# # functools
+# # Functools module is designed for higher-order functions:
+# # functions that act on or return other functions
+# from functools import lru_cache
+# import requests
+#
+#
+# # Two types of decorators for cache
+# # First tpe - lru
+# @lru_cache(maxsize=32)
+# def get_with_cache(url):
+#     try:
+#         r = requests.get(url)
+#         return r.text
+#     except:
+#         return "Not Found"
+#
+#
+# for url in ['https://ya.ru/',
+#             'https://google.com/',
+#             'https://msn.com']:
+#     get_with_cache(url)
+#
+# print(get_with_cache.cache_info())
+#
+# # Second type - cached property
+# from functools import cached_property
+#
+#
+# class Page:
+#     @cached_property
+#     def render(self, value):
+#         # do somth high-load and as fact for showing html-page
+#         return html
+#
+#
+# functools have a lot of methods for different cases like
+# Automatic implementation of comparison operators
+# from functools import total_ordering
+# Overloading methods/functions
+# from functools import singledispatch
+# from functools import singledispatchmethod
+# Passing callback functions
+# from functools import partial
+# Saving the name and documentation lines of the decorator function
+# from functools import wraps
+# Reduce func - sum items of iter object or somth like that
+# from functools import
+#
+# # -----------------------------
 
+# # heapq
+# # Quick and easy way to create any type of priority queue for your application
+#
+# import heapq
+#
+# a = [1, 6, 4, 3, 2]
+# heapq.heapify(a)
+# print('Heap =', a)
+# heapq.heappush(a, 10)
+# print(a)
+# poped_element = heapq.heappop(a)
+# print(poped_element)
+# #
+# # # -----------------------------
 
-# example2
-def grade(score, breakpoints=[30, 40, 50, 60], grades='ABC'):
-    i = bisect(breakpoints, score)
-    return grades[i]
+# copy
+# For light and deep copy of object
 
+import copy
 
-print([grade(score) for score in [32, 43, 11, 40, 20, 200]])
+data = {1: 5, 2: 6, 3: 7, 4: 8, 5: {10: 11}}
+new_data = copy.copy(data)
+data[7] = 99
+data[8] = 101
+data[5][10] = 111
+print(data)
+print(new_data)
 
-# -----------------------------
+new_data_deep = copy.deepcopy(data)
+data[5][10] = 112
+print(new_data_deep)
+#
+# # -----------------------------
