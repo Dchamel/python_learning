0ceaac23d0ffc42e0e06d37eebe0e81c1bcf9558
dchamel <dchamel@yandex.ru>
2024-08-29 11:14:28 +0400

    SB - Libs for import libs

--- Python Additions/Build-In modules/libs_for_handle_outer_data.py
+++ Python Additions/Build-In modules/libs_for_handle_outer_data.py
@@ -1,15 +1,33 @@
 # ---
+
 # json
 
-import json
-
-with open('data.json', 'w') as write_file:
-    json.dump(data, write_file, indent=4)
-
-json_string = json.dumps(data)
-
-with open('data.json', 'r') as read_file:
-    data = json.load(read_file)
-data = json.loads(json_string)
+# import json
+#
+# with open('data.json', 'w') as write_file:
+#     json.dump(data, write_file, indent=4)
+#
+# json_string = json.dumps(data)
+#
+# with open('data.json', 'r') as read_file:
+#     data = json.load(read_file)
+# data = json.loads(json_string)
 
 # ---
+
+# csv - reads and writes table data to csv
+import csv
+
+with open('sw_data.csv', newline='') as csvfile:
+    reader = csv.reader(f1, delimiter=';')
+    headers = next(reader)
+    print(headers)
+
+    for row in reader:
+        print(row)
+
+with open('sw_data.csv', newline='') as csvfile2:
+    reader = csv.DictReader(csvfile2)
+    for row in reader:
+        print(row)
+        print(row['hostname'], row['model'])
