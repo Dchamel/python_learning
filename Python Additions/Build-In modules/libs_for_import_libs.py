320875fdef65e45dc92753a56c8936c040e992c7
dchamel <dchamel@yandex.ru>
2024-08-29 10:20:31 +0400

    SB - Libs for import libs

--- Python Additions/Build-In modules/libs_for_import_libs.py
+++ Python Additions/Build-In modules/libs_for_import_libs.py
@@ -5,24 +5,29 @@ import importlib
 importlib.import_module('some_module')
 
 # ---
-# Check module importing
-# "EAFP: Easier to ask for forgiveness than permission"
-
-import importlib.util
-
-
-def check_module(module_name):
-    """Check module importing without import it"""
-    module_spec = importlib.util.find_spec(module_name)
-    if module_spec is None:
-        print('Module not found: {}'.format(module_name))
-        return False
-    else:
-        print('Module found: {}'.format(module_name))
-        return module_spec
-
+# # Check module importing
+# # "EAFP: Easier to ask for forgiveness than permission"
+#
+# import importlib.util
+#
+#
+# def check_module(module_name):
+#     """Check module importing without import it"""
+#     module_spec = importlib.util.find_spec(module_name)
+#     if module_spec is None:
+#         print('Module not found: {}'.format(module_name))
+#         return False
+#     else:
+#         print('Module found: {}'.format(module_name))
+#         return module_spec
 # ---
+import ctypes
 
+# Load libs to ctypes
+libc = ctypes.CDLL('/libclib.so')
+
+# Load C-func from a lib
+counter = libc.simple_function()
 
 # -----------------------
 
