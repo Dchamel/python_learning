4ce7edc63b3a228916a45814e17e222a1a8d0fd3
dchamel <dchamel@yandex.ru>
2025-06-18 10:37:21 +0400

    Retrievers Additions

--- MyProjects/GigachatAPI/retrievers_techs.py
+++ MyProjects/GigachatAPI/retrievers_techs.py
@@ -24,7 +24,7 @@ db = Chroma.from_documents(docs, embeddings)
 # retrieved_docs = retriever.invoke('Что сказала Анна ?')
 # print(retrieved_docs[0].page_content)
 
-# Base Retriever
+# todo Base Retriever
 # 'k' - docs number in return
 # 'score_threshold' - порог схожести для расстояния между эмбедингами, в результаты попадут только дки, для которых значение метрики расстояния до запроса превышает порог
 
@@ -36,13 +36,13 @@ retriever = db.as_retriver(
 )
 docs = retriever.get_relevant_documents('Кто такая Анна ?')
 
-# MMR (Maximum Marginal Relevance)
+# todo MMR (Maximum Marginal Relevance)
 # Если в бвзе дубли - MMR, алгоритм поиска и ранжирования. Он учитывает релевантность каждого документа и "новизну" информации в нём относительно отобранных ранее.
 # MMR позволяет получить релевантные и при этом максимально насыщенные информацие доки без дублирования
 
 retriever = db.as_retriver(search_type='mmr')
 
-# MultiQuery
+# todo MultiQuery
 # MQR - для получения максимально релеванной информации и полной по отдельному запросу. Расширяет запрос, генерируя похожие на него по смыслу.
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.chat_models import GigaChat
@@ -55,7 +55,7 @@ retriever_from_llm = MultiQueryRetriever.from_llm(
 )
 
 # HyDE
-# Hypothetical Document Embeddings
+# todo Hypothetical Document Embeddings
 # Поиск релевантных документов и генерация гипотетического, необязательно достоверного
 from langchain.chains import HypotheticalDocumentEmbedder
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
@@ -69,3 +69,46 @@ db = Chroma.from_texts(texts, embeddings)
 
 query = 'Кто есть Анна ?'
 docs = db.similarity_search(query)
+
+# todo Reranking
+# На HuggingFace есть модель bge-reranker-large скрипт реализующий cross-encoder ?
+class BgeRerank(BaseDocumentCompressor):
+    model_name: str = 'BAAI/bge-reranker-large'
+    top_n: int = 5
+
+    model.CrossEncoder = CrossEncoder(model_name=model_name)
+
+    def bge_rerank(self.query, documents):
+        model_inputs = [[query, doc] for doc in docs]
+        scores = self.model.predict(model_inputs)
+        results = sorted(enumerate(scores), key=lambda x: x[1], reverse=True)
+
+        return results[:self.top_n]
+
+    def compress_documents(
+            self,
+            documents: Sequence[Document],
+            query: str,
+            callbacks: Optional[Callbacks] = None
+    ) -> Sequence[Document]:
+
+        if(len(documents) == 0):
+            return []
+
+        doc_list = list(documents)
+        _docs = [d.page_content for d in doc_list]
+        results = self.bge_rerank(query, _docs)
+        final_results = []
+
+        for r in results:
+            doc = doc_list[r[0]]
+            doc.metadata["relevance_score"] = r[1]
+            final_results.append(doc)
+
+        return final_results
+
+# full pipeline
+first_stage_reranker = chroma.as_retriever(search_kwargs={'k':100})
+first_stage_results = first_stage_reranker.get_relevant_documents(query)
+
+second_stage_results = bge_reranker.compress_documents(first_stage_results, query)
